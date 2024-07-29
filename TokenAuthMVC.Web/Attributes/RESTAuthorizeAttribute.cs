7c1a95282affdb9f4328edf578a6fcf313898b66
JP Firstapp <jp@firstapp.com>
2024-07-29 15:09:23 +0530

    Initial commit

---
+++ TokenAuthMVC.Web/Attributes/RESTAuthorizeAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using TokenAuthMVC.Managers;
+
+namespace TokenAuthMVC.Attributes
+{
+    /// <summary>
+    /// Token-based authentication for ASP .NET MVC REST web services.
+    /// Copyright (c) 2015 Kory Becker
+    /// http://primaryobjects.com/kory-becker
+    /// License MIT
+    /// </summary>
+    public class RESTAuthorizeAttribute : AuthorizeAttribute
+    {
+        private const string _securityToken = "token";
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (Authorize(filterContext))
+            {
+                return;
+            }
+
+            HandleUnauthorizedRequest(filterContext);
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            base.HandleUnauthorizedRequest(filterContext);
+        }
+
+        private bool Authorize(AuthorizationContext actionContext)
+        {
+            try
+            {
+                HttpRequestBase request = actionContext.RequestContext.HttpContext.Request;
+                string token = request.Params[_securityToken];
+
+                return SecurityManager.IsTokenValid(token, CommonManager.GetIP(request), request.UserAgent);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
